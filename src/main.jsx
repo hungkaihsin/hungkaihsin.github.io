44a6c65a98bd8803e48f0dd42df9d6d3af9868b4
hungkaihsin <k_hung2@u.pacific.edu>
2025-10-31 17:05:30 -0700

    feat: add admin page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import AdminPage from './pages/AdminPage/AdminPage.jsx';
+import ChangePasswordPage from './pages/ChangePasswordPage/ChangePasswordPage.jsx';
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: '/admin',
     element: <AdminPage />,
+  },
+  {
+    path: '/change-password',
+    element: <ChangePasswordPage />,
   },
 ]);
 
